772e54c7464d49c4f528bc75a3b8ccb57b604792
Richa Tiwary <52816720+richatiwary1911@users.noreply.github.com>
2022-03-17 01:32:21 +0530

    Time: 5 ms (82.75%), Space: 47.4 MB (61.00%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -1,34 +1,73 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         
+        //using Kahn's algo
+        
+//         ArrayList<Integer> graph[] = new ArrayList[numCourses];        
+//         for(int i = 0; i < numCourses; i++)
+//             graph[i] = new ArrayList<>();
+        
+//         int inDegree[] = new int[numCourses];
+//         for(int p[] : prerequisites)
+//         {
+//             graph[p[0]].add(p[1]);
+//             inDegree[p[1]] ++;
+//         }
+        
+//         Queue<Integer> que = new LinkedList<>();
+        
+//         for(int i = 0; i < numCourses; i++)
+//             if(inDegree[i] == 0)
+//                 que.add(i);
+        
+//         int count = 0;
+        
+//         while(!que.isEmpty())
+//         {
+//             int rem = que.remove();
+//             count++;
+            
+//             for(int nbr : graph[rem])
+//                 if(--inDegree[nbr] == 0)
+//                     que.add(nbr);
+//         }
+//         return numCourses == count;
+ 
+//-----------------------------------------------------------------------------------------------------------------        
+        //cycle detection using DFS
         ArrayList<Integer> graph[] = new ArrayList[numCourses];        
         for(int i = 0; i < numCourses; i++)
             graph[i] = new ArrayList<>();
         
-        int inDegree[] = new int[numCourses];
+        //int inDegree[] = new int[numCourses];
         for(int p[] : prerequisites)
         {
             graph[p[0]].add(p[1]);
-            inDegree[p[1]] ++;
+            //inDegree[p[1]] ++;
         }
         
-        Queue<Integer> que = new LinkedList<>();
-        
+        int state[] = new int[numCourses];
+        List<Integer> path = new ArrayList<>();
         for(int i = 0; i < numCourses; i++)
-            if(inDegree[i] == 0)
-                que.add(i);
+            if(state[i] == 0)
+                dfs(graph, i, state, path);
         
-        int count = 0;
-        
-        while(!que.isEmpty())
+        return path.size() == numCourses;
+    }
+    
+    public boolean dfs(ArrayList<Integer> graph[], int src, int state[], List<Integer> path)
+    {
+        state[src] = 1;
+        for(int nbr : graph[src])
         {
-            int rem = que.remove();
-            count++;
+            if(state[nbr] == 1)
+                return false;
             
-            for(int nbr : graph[rem])
-                if(--inDegree[nbr] == 0)
-                    que.add(nbr);
+            if(state[nbr] == 0)
+                if(!dfs(graph, nbr, state, path)) return false;
         }
-        return numCourses == count;
+        state[src] = 2;
+        path.add(src);
+        return true;
     }
 }
