3c5773f8fc74792837eac9eb141bd2f32a11beb9
Richa Tiwary <52816720+richatiwary1911@users.noreply.github.com>
2022-05-29 16:16:54 +0530

    Time: 1 ms (36.54%), Space: 45.5 MB (62.28%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -18,11 +18,12 @@ public class Solution {
         ListNode fast = head;
         
         while(fast.next != null && fast.next.next != null) {
+            
             slow = slow.next;
             fast = fast.next.next;
             
             if(slow == fast) return true;
         }
-        return false;        
+        return false;     
     }
 }
